bd38aa94509dde5fcc35b3f821bd41cc9bf15de7
showrav38 <showravsaha38@gmail.com>
2022-08-18 19:48:57 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Home/>
+      </Route>
+      <Route path="/dashboard" exact>
+        <Dashboard/>
+      </Route>
+    </Router>
   );
 }
 
